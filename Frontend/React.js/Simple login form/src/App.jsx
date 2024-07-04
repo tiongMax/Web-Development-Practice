85ae850014c395c0d36c461d8167395f036b5938
tiongMax <maxtiongcs16@gmail.com>
2024-07-05 00:39:25 +0800

    Setup files and structure

---
+++ Frontend/React.js/Simple login form/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+import LoginSignUp from './Components/Login-signup-component/LoginSignup'
+
+export default function App() {
+
+  return (
+    <div>
+      <LoginSignUp />
+    </div>
+  )
+}
---
+++ Frontend/React.js/Simple login form/src/Components/Login-signup-component/LoginSignUp.jsx
@@ -0,0 +1,9 @@
+import LoginSignUp from "./LoginSignUp.css";
+
+import user_icon from "../Assets/person.png"
+import email_icon from "../Assets/email.png"
+import password_icon from "../Assets/password.png"
+
+export default function LoginSignUp() {
+    return 
+}
