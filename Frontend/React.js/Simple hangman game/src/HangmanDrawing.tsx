56720f78153a27ba8e800895a4256472ba3f4e69
tiongMax <maxtiongcs16@gmail.com>
2024-07-25 23:15:44 +0800

    Add body to the hangman

--- Frontend/React.js/Simple hangman game/src/HangmanDrawing.tsx
+++ Frontend/React.js/Simple hangman game/src/HangmanDrawing.tsx
@@ -1,24 +1,24 @@
 const HEAD: JSX.Element = (
     <div style={{
-        width: "50px",
-        height: "50px",
-        borderRadius: "100%",
-        border: "10px solid black",
-        position: "absolute",
-        top: "50px",
-        right: "-30px"
+            width: "50px",
+            height: "50px",
+            borderRadius: "100%",
+            border: "10px solid black",
+            position: "absolute",
+            top: "50px",
+            right: "-30px"
         }}
     />
 )
 
 const BODY: JSX.Element = (
     <div style={{
-        width: "10px",
-        height: "100px",
-        background: "black",
-        position: "absolute",
-        top: "50px",
-        right: "-30px"
+            width: "10px",
+            height: "100px",
+            background: "black",
+            position: "absolute",
+            top: "118px",
+            right: "0px"
         }}
     />
 )
@@ -27,6 +27,7 @@ export default function HangmanDrawing(): JSX.Element {
     return (
         <div style={{ position: "relative" }}>
             {HEAD}
+            {BODY}
             <div style={{
                     height: "50px",
                     width: "10px",
